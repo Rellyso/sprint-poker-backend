8c1a9e6316c54cfa4aeec17d90bd4d7e0a17a6b0
Rellyson <rellysondouglas2015@gmail.com>
2024-11-30 09:35:09 -0300

    fix: github login

--- src/auth/passport-config.ts
+++ src/auth/passport-config.ts
@@ -53,6 +53,7 @@ passport.use(
       clientID: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
       callbackURL: "/api/auth/github/callback",
+      proxy: true,
     },
 
     async (
